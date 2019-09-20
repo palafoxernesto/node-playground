782d302727276053a71238a69e79ccb6d4e2b4ae
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-09-20 02:33:57 -0500

    Create missing CRUD enpoints

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -16,6 +16,53 @@ function moviesApi (app) {
       next(err)
     }
   })
+  router.get('/:movieId', async function (req, res, next) {
+    try {
+      const movie = await Promise.resolve(moviesMock[0])
+      res.status(200).json({
+        data: movie,
+        message: 'movie retrived'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.post('/', async function (req, res, next) {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id)
+      res.status(200).json({
+        data: createdMovieId,
+        message: 'movie created'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.put('/:movieId', async function (req, res, next) {
+    try {
+      const updatedMovie = await Promise.resolve(moviesMock[0].id)
+      res.status(201).json({
+        data: updatedMovie,
+        message: 'movie updated'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.delete('/:movieId', async function (req, res, next) {
+    try {
+      const deletedMovie = await Promise.resolve(moviesMock[0].id)
+      res.status(200).json({
+        data: deletedMovie,
+        message: 'movie deleted'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
 }
 
 module.exports = {
