fc5bfdb58eeb630ef9f9c88b24eb982ca96d6b75
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-09-20 22:48:58 -0500

    Creates caheResponse - time constants - routes cached

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -7,6 +7,11 @@ const {
   updateMoveSchema
 } = require('../utils/schemas/movies')
 const validationHandler = require('../utils/middleware/validationHandler')
+const cacheResponse = require('../utils/cacheResponse')
+const {
+  FIVE_MINUTES_IN_SECONDS,
+  SIXTY_MINUTES_IN_SECONDS
+} = require('../utils/time')
 
 function moviesApi (app) {
   const router = express.Router()
@@ -14,6 +19,7 @@ function moviesApi (app) {
 
   const moviesService = new MoviesService()
   router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
     const { tags } = req.query
     try {
       const movies = await moviesService.getMovies({ tags })
@@ -28,6 +34,7 @@ function moviesApi (app) {
   router.get('/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function (req, res, next) {
+      cacheResponse(res, SIXTY_MINUTES_IN_SECONDS)
       const { movieId } = req.params
       try {
         const movie = await moviesService.getMovie({ movieId })
